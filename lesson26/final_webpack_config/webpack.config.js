6bed700edbf9de57304537d0ad41dfdc84b4dbc0
Vova <v.kalynii@gmail.com>
2021-03-13 12:06:32 +0200

    инициализация

---
+++ lesson26/final_webpack_config/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const conf = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, './dist'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 1234,
+    },
+};
+
+module.exports = (env, options) => {
+    const isProd = options.mode === 'production';
+    conf.devtool = isProd ? false : 'eval-cheap-module-source-map';
+    conf.target = isProd ? 'browserslist' : 'web';
+
+    return conf;
+};
