131fc18fe9a1301b73661e9cae5f4f480e390c4e
Vova <v.kalynii@gmail.com>
2021-03-15 21:03:20 +0200

    Update js.js

--- lesson25/node-test-project/src/js/js.js
+++ lesson25/node-test-project/src/js/js.js
@@ -1,1 +1,28 @@
-console.log('Привет');
+// import 'jquery';
+
+const text = 'Привет мир';
+
+console.log(text);
+
+const user = {
+	name: 'Ivan'
+};
+
+
+const arr = [1,2,3,4,5];
+
+
+const newArr = arr.map(item => item)
+	.map(item => item)
+	.map(item => item)
+	.map(item => item)
+	.map(item => item)
+	.map(item => item)
+	.map(item => item)
+	.map(item => item);
+
+
+const doValue = (val) => val * 2;
+
+console.log(doValue(5));
+// $('#app').append(doValue);
