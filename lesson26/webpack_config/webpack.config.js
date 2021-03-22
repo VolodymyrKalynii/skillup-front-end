43fd20936a4a37940963e9494fe7fd82a90faed0
Volodymyr <v.kalynii@gmail.com>
2021-03-22 19:36:58 +0200

    Настройка точки входа, выхода, и добавление HtmlWebpackPlugin

---
+++ lesson26/webpack_config/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+
+const conf = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+    ],
+
+}
+
+
+
+
+module.exports = () => { return conf };
