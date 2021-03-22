8a9eff0182813cbaae5b3176c6e0a8f19f71edd1
Volodymyr <v.kalynii@gmail.com>
2021-03-22 20:41:11 +0200

    Импорт User и functions

--- lesson26/webpack_config/src/index.js
+++ lesson26/webpack_config/src/index.js
@@ -1,30 +1,21 @@
+import User from './User.js';
+import {multThree, multTwo} from './functions';
+
 console.log('hello word');
 
 const foo = (name) => {
     console.log(`hello ${name}`);
 }
 
-
 foo('asdad');
 foo('Test');
 
-
 document.querySelector('#app').innerHTML = 'Привет большой мир';
 
-
-class User {
-    constructor(name) {
-        this.__name = name;
-
-    }
-    printName() {
-        console.log(this.__name);
-    }
-}
 
 const john = new User('John');
 
 john.printName();
 
-
-// .innerHTML('asdasdada')
+console.log(multThree(3));
+console.log(multTwo(5));
