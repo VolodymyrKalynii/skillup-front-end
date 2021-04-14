3dbb16a68223821c1f4a0c668ba7bd5f38550eea
Volodymyr <v.kalynii@gmail.com>
2021-04-14 21:23:41 +0300

    Update Nav.jsx

--- src/components/common/nav/Nav.jsx
+++ src/components/common/nav/Nav.jsx
@@ -3,10 +3,10 @@ import {NavLink} from 'react-router-dom';
 
 export const Nav = () => (
     <div>
-        <NavLink activeStyle={{color: 'red'}} to='/'> на главную</NavLink>
+        <NavLink exact activeStyle={{color: 'red'}} to='/'> на главную</NavLink>
         <NavLink activeStyle={{color: 'red'}} to='/planets'> planets</NavLink>
         <NavLink activeStyle={{color: 'red'}} to='/comments'> комменты</NavLink>
-        <NavLink activeStyle={{color: 'red'}} to='/products'> Продукты</NavLink>
+        <NavLink exact activeStyle={{color: 'red'}} to='/products'> Продукты</NavLink>
         <NavLink activeStyle={{color: 'red'}} to='/products/1'> Телевизор</NavLink>
         <NavLink activeStyle={{color: 'red'}} to='/products/2'> Диван</NavLink>
         {/* <a href='/'> на главную</a> */}
